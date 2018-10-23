ae7e10b37f96f7cd02b4a7e87518779130742ad7
majun2232 <253495832@qq.com>
2018-10-23 15:27:19 +0800

    文字模板分离完毕

--- src/components/wordsTem.vue
+++ src/components/wordsTem.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div>{{inWordDate.text}}</div>
+        <div>{{inWodTmp.text}}</div>
         <span v-for="(item,index) of Preface" :key="index">
             <span> {{item.text}}为</span>
             <span v-if=item.unit> {{item.A}} </span>
@@ -12,6 +12,9 @@
 </template>
 
 <script>
+/* 这是一个生成文字报表的子组件,需要使用时,父组件绑定  :inWordDate='inWordDate' :inWodTmp='inWodTmp'  这样的格式
+需要传一个模板文字和一个对应的数据,在里面会用 this.$set进行数据拼接,传的对象格式为如下,本模板会自动解析传入的数据 
+ */
     export default {
         name: '',
         data() {
@@ -23,29 +26,31 @@
                 // unit: '',
             }
         },
-        props: ['inWordDate'],
+        props: ['inWordDate','inWodTmp'],
         created() {
-            //   下面是数据文件
-             var pre =this.inWordDate
-            //  下面是文字模板文件数据
-            this.Preface = this.inWordDate.model.tables[0].row;
-            console.log(this.Preface);
-            for (let index = 0; index < this.Preface.length; index++) {
-                const ele = this.Preface[index];
-                // console.log(ele);
-                for (let j = 0; j < pre.length; j++) {
-                    // debugger
-                    const element = pre[j];
-                    if (ele.id === element.id) {
-                        if (element.B > 100) {
-                            this.Preface.A.push((element.B / 10000).toFixed(2))
-                            break;
-                        } else
-                            this.Preface.A.push(element.B.toFixed(2))
-                        break;
-                    }
+       var pre =this.inWordDate
+          this.Preface = this.inWodTmp
+          // console.log(this.Preface);
+          for (let index = 0; index < this.Preface.length; index++) {
+            const ele = this.Preface[index];
+            // console.log(ele);
+            for (let j = 0; j < pre.length; j++) {
+              const element = pre[j];
+              // console.log(element);
+              if (ele.id === element.id) {
+                if (element.B > 100) {
+                    // 下面对原数据进行拼接一个A进去
+                  this.$set(this.Preface[index], "A", (element.B / 10000).toFixed(2))
+                //   console.log(this.Preface);
+                  break;
+                } else {
+                  this.$set(this.Preface[index], "A", element.B.toFixed(2))
+                  break;
                 }
+              }
             }
+          }
+  
         }
     }
 </script>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,28 +1,18 @@
 <template>
   <div>
-    <!-- <words-tem :inWordDate='tableData' v-if="flag"></words-tem> -->
-    <!-- <table-show :incomeDate='tableData' v-if="flag"></table-show> -->
-    <!-- <div>{{inWordDate.text}}</div> -->
-    <span v-for="(item,index) of Preface" :key="index">
-      <span> {{item.text}}为</span>
-      <span v-if=item.unit> {{item.A}} </span>
-      <span v-else> {{item.A}}万元 </span>
-      <span>{{item.unit}},</span>
-    </span>
-
-
-
+    <words-tem :inWordDate='inWordDate' :inWodTmp='inWodTmp'  v-if="flag"></words-tem> 
   </div>
 </template>
 
 <script>
   // import tableShow from '@c/tableTem'
-  // import wordsTem from '@c/wordsTem'
+  import wordsTem from '@c/wordsTem'
   export default {
     name: '',
     data() {
       return {
-        LiteralData: [],
+     inWordDate: [],
+     inWodTmp:[],
         PrefaceData: [],
         Preface: [],
         tableData: [],
@@ -31,48 +21,17 @@
     },
     components: {
       // tableShow,
-      // wordsTem
+      wordsTem
     },
     created() {
       var pre = []
-
       this.$axios.get("/api/PrefaceData").then(res => {
-          // pre = res.data;
-          // console.log(pre);
-
+          this.inWordDate = res.data;
+     
         }),
         this.$axios.get("/api/Preface").then(res => {
-          this.LiteralData = res.data
-          this.Preface = res.data.model.tables[0].row;
-          // console.log(this.Preface);
-          for (let index = 0; index < this.Preface.length; index++) {
-
-            const ele = this.Preface[index];
-            // console.log(ele);
-            for (let j = 0; j < pre.length; j++) {
-              // debugger
-              const element = pre[j];
-              console.log(element);
-
-              if (ele.id === element.id) {
-                if (element.B > 100) {
-                  // debugger
-                  this.$set(this.Preface, "A", (element.B / 10000).toFixed(2))
-                  console.log(this.Preface);
-
-                  break;
-                } else {
-                  // this.Preface.A.push(element.B.toFixed(2))
-                  this.$set(this.Preface, "A", element.B.toFixed(2))
-                  break;
-                }
-
-              }
-            }
-
-
-          }
-          console.log(this.Preface);
+          this.flag=true;
+          this.inWodTmp = res.data.model.tables[0].row;
         })
       // this.$axios.get("/api/10101/201803/0002").then(res => {
 
